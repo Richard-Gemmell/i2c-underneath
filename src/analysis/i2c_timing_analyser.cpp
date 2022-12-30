749e7aa28990a802a23f5dea2f43fa55ee1d5167
Richard Gemmell <16683467+Richard-Gemmell@users.noreply.github.com>
2022-12-30 16:39:45 +0000

    Refactord TimingAnalyser.

--- src/analysis/i2c_timing_analyser.cpp
+++ src/analysis/i2c_timing_analyser.cpp
@@ -7,6 +7,7 @@ namespace analysis {
 I2CTimingAnalysis I2CTimingAnalyser::analyse(const bus_trace::BusTrace& trace,
                                              uint16_t sda_rise_time, uint16_t scl_rise_time,
                                              uint16_t sda_fall_time, uint16_t scl_fall_time) {
+    Adjuster adjust(sda_rise_time, sda_fall_time, scl_rise_time, scl_fall_time);
     // TODO: check that the trace is well formed
     // maybe get the trace to normalise itself first or maybe that's up to the caller
 //    bool well_formed = true;
@@ -32,17 +33,17 @@ I2CTimingAnalysis I2CTimingAnalyser::analyse(const bus_trace::BusTrace& trace,
         auto previous_event = trace.event(current_edge - 1);
         auto current_event = trace.event(current_edge);
         auto flags = current_event->flags;
-        if(flags & bus_trace::BusEventFlags::SCL_LINE_CHANGED) {
+        if (flags & bus_trace::BusEventFlags::SCL_LINE_CHANGED) {
             // SCL changed
-            if(current_event->scl_rose()) {
+            if (current_event->scl_rose()) {
                 // SCL LOW -> HIGH
                 previous_scl_rise_event = current_edge;
                 latest_clock_low = trace.nanos_between(current_edge, previous_scl_fall_event);
 //                Serial.printf("Index %d LOW time %d\n", current_edge, latest_clock_low);
-                scl_low_time_stats.include(adjust_clock_low_time(latest_clock_low, scl_fall_time, scl_rise_time));
+                scl_low_time_stats.include(adjust.clock_low_time(latest_clock_low));
                 if (data_changed) {
                     size_t raw_setup_data_time = trace.nanos_to_previous(current_edge);
-                    data_setup_time_stats.include(adjust_data_setup_time(raw_setup_data_time, previous_event->sda_rose(), sda_rise_time, sda_fall_time, scl_rise_time));
+                    data_setup_time_stats.include(adjust.data_setup_time(raw_setup_data_time, previous_event->sda_rose()));
                     data_changed = false;
                 }
             } else {
@@ -52,40 +53,38 @@ I2CTimingAnalysis I2CTimingAnalyser::analyse(const bus_trace::BusTrace& trace,
                     // This is a data bit or a NACK/ACK
                     auto clock_high = trace.nanos_between(current_edge, previous_scl_rise_event);
 //                    Serial.printf("Index %d HIGH time %d\n", current_edge, clock_high);
-                    scl_high_time_stats.include(adjust_clock_high_time(clock_high, scl_rise_time, scl_fall_time));
+                    scl_high_time_stats.include(adjust.clock_high_time(clock_high));
 
                     // Calculate frequency for the last clock cycle
                     auto period = clock_high + latest_clock_low;
                     auto frequency = (uint32_t)((1e9 * 1.0) / period);
 //                    Serial.printf("Index %d period %d frequency %d\n", current_edge, period, frequency);
                     clock_frequency_stats.include(frequency);
-                }
-                else if(previous_event->sda_fell()) {
+                } else if (previous_event->sda_fell()) {
                     // SCL HIGH -> LOW after SDA fell. This is a START condition.
-                    uint32_t start_hold_time = adjust_start_hold_time(trace.nanos_to_previous(current_edge), sda_fall_time, scl_fall_time);
+                    uint32_t start_hold_time = adjust.start_hold_time(trace.nanos_to_previous(current_edge));
                     start_hold_time_stats.include(start_hold_time);
-                }
-                else {
+                } else {
                     Serial.println("Invalid Trace: Found SCL falling edge but not a data bit or STOP condition.");
                 }
             }
         } else {
             // SDA changed
-            if(flags & bus_trace::BusEventFlags::SCL_LINE_STATE) {
+            if (flags & bus_trace::BusEventFlags::SCL_LINE_STATE) {
                 // SDA LOW -> HIGH while SCL is HIGH. This is a STOP or START condition.
                 if (current_event->sda_rose()) {
                     // SDA LOW -> HIGH while SCL is HIGH. This is a STOP condition.
-                    uint32_t setup_stop_time = adjust_setup_stop_time(trace.nanos_to_previous(current_edge), sda_rise_time, scl_rise_time);
+                    uint32_t setup_stop_time = adjust.setup_stop_time(trace.nanos_to_previous(current_edge));
                     stop_setup_time_stats.include(setup_stop_time);
                 } else {
                     // SDA HIGH -> LOW while SCL is HIGH. This is a START condition.
                     if (previous_event->scl_rose()) {
                         // This is a repeated START condition
-                        uint32_t setup_start_time = adjust_setup_start_time(trace.nanos_to_previous(current_edge), sda_fall_time, scl_rise_time);
+                        uint32_t setup_start_time = adjust.setup_start_time(trace.nanos_to_previous(current_edge));
                         start_setup_time_stats.include(setup_start_time);
                     } else if (previous_event->sda_rose()) {
                         // This is START following a STOP
-                        uint32_t bus_free_time = adjust_bus_free_time(trace.nanos_to_previous(current_edge), sda_rise_time, sda_fall_time);
+                        uint32_t bus_free_time = adjust.bus_free_time(trace.nanos_to_previous(current_edge));
                         bus_free_time_stats.include(bus_free_time);
                     } else if (previous_event->flags == (bus_trace::BusEventFlags::SDA_LINE_STATE | bus_trace::BusEventFlags::SCL_LINE_STATE)) {
                         // This is START at the beginning of a trace
@@ -117,54 +116,61 @@ I2CTimingAnalysis I2CTimingAnalyser::analyse(const bus_trace::BusTrace& trace,
     };
 }
 
+I2CTimingAnalyser::Adjuster::Adjuster(uint16_t sda_rise_time, uint16_t sda_fall_time,
+                                      uint16_t scl_rise_time, uint16_t scl_fall_time)
+    : sda_rise_time(sda_rise_time),
+      sda_fall_time(sda_fall_time),
+      scl_rise_time(scl_rise_time),
+      scl_fall_time(scl_fall_time) {
+}
+
 // The Teensy triggers too soon when SCL rises and too late when SDA rises.
-uint32_t I2CTimingAnalyser::adjust_setup_stop_time(uint32_t raw_time, uint16_t sda_rise_time, uint16_t scl_rise_time) {
-    auto adjusted =  (uint32_t)(raw_time - (scl_rise_time * RISE_TRIGGER_to_V0_7) - (sda_rise_time * RISE_V0_3_to_TRIGGER));
+uint32_t I2CTimingAnalyser::Adjuster::setup_stop_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - scl_rise_trigger_to_V0_7() - sda_rise_V0_3_to_trigger());
 //    Serial.printf("Setup stop time (tSU;STO) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
 // The Teensy triggers too soon when SCL rises and too late when SDA falls.
-uint32_t I2CTimingAnalyser::adjust_setup_start_time(uint32_t raw_time, uint16_t sda_fall_time, uint16_t scl_rise_time) {
-    auto adjusted = (uint32_t)(raw_time - (scl_rise_time * RISE_TRIGGER_to_V0_7) - (sda_fall_time * FALL_V0_7_to_TRIGGER));
+uint32_t I2CTimingAnalyser::Adjuster::setup_start_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - scl_rise_trigger_to_V0_7() - sda_fall_V0_7_to_trigger());
 //    Serial.printf("Setup time for repeated start (tSU;STA) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
-// Adjusts for the fact that the Teensy triggers too soon when SDA falls and too late when SCL falls.
-uint32_t I2CTimingAnalyser::adjust_start_hold_time(uint32_t raw_time, uint16_t sda_fall_time, uint16_t scl_fall_time) {
-    auto adjusted = (uint32_t)(raw_time - (sda_fall_time * FALL_TRIGGER_to_V0_3) - (scl_fall_time * FALL_V0_7_to_TRIGGER));
+// The Teensy triggers too soon when SDA falls and too late when SCL falls.
+uint32_t I2CTimingAnalyser::Adjuster::start_hold_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - sda_fall_trigger_to_V0_3() - scl_fall_V0_7_to_trigger());
 //    Serial.printf("Start hold (tHD;STA) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
 // Adjusts for the fact that the Teensy triggers at 0.5 Vdd
-uint32_t I2CTimingAnalyser::adjust_clock_low_time(uint32_t raw_time, uint16_t scl_fall_time, uint16_t scl_rise_time) {
-    auto adjusted = (uint32_t)(raw_time - (scl_fall_time * FALL_TRIGGER_to_V0_3) - (scl_rise_time * RISE_V0_3_to_TRIGGER));
+uint32_t I2CTimingAnalyser::Adjuster::clock_low_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - scl_fall_trigger_to_V0_3() - scl_rise_V0_3_to_trigger());
 //    Serial.printf("Clock low (tLOW) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
 // Adjusts for the fact that the Teensy triggers at 0.5 Vdd
-uint32_t I2CTimingAnalyser::adjust_clock_high_time(uint32_t raw_time, uint16_t scl_rise_time, uint16_t scl_fall_time) {
-    auto adjusted = (uint32_t)(raw_time - (scl_rise_time * RISE_TRIGGER_to_V0_7) - (scl_fall_time * FALL_V0_7_to_TRIGGER));
+uint32_t I2CTimingAnalyser::Adjuster::clock_high_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - scl_rise_trigger_to_V0_7() - scl_fall_V0_7_to_trigger());
 //    Serial.printf("Clock high (tHIGH) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
-uint32_t I2CTimingAnalyser::adjust_bus_free_time(uint32_t raw_time, uint16_t sda_rise_time, uint16_t sda_fall_time) {
-    auto adjusted = (uint32_t)(raw_time - (sda_rise_time * RISE_TRIGGER_to_V0_7) - (sda_fall_time * FALL_V0_7_to_TRIGGER));
+uint32_t I2CTimingAnalyser::Adjuster::bus_free_time(uint32_t raw_time) const {
+    auto adjusted = (uint32_t)(raw_time - sda_rise_trigger_to_V0_7() - sda_fall_V0_7_to_trigger());
 //    Serial.printf("Bus Free Time (tBUF) raw: %d adjusted %d\n", raw_time, adjusted);
     return adjusted;
 }
 
-uint32_t I2CTimingAnalyser::adjust_data_setup_time(uint32_t raw_time, bool sda_rose, uint16_t sda_rise_time,
-                                                   uint16_t sda_fall_time, uint16_t scl_rise_time) {
-    auto adjusted = raw_time - (scl_rise_time * RISE_V0_3_to_TRIGGER);
-    if(sda_rose) {
-        adjusted = adjusted - (sda_rise_time * RISE_TRIGGER_to_V0_7);
+uint32_t I2CTimingAnalyser::Adjuster::data_setup_time(uint32_t raw_time, bool sda_rose) const {
+    auto adjusted = raw_time - scl_rise_V0_3_to_trigger();
+    if (sda_rose) {
+        adjusted = adjusted - sda_rise_trigger_to_V0_7();
     } else {
-        adjusted = adjusted - (sda_fall_time * FALL_TRIGGER_to_V0_3);
+        adjusted = adjusted - sda_fall_trigger_to_V0_3();
     }
     auto result = (uint32_t)adjusted;
 //    Serial.printf("Data Setup Time (tSU;DAT) raw: %d adjusted %d\n", raw_time, result);
--- src/analysis/i2c_timing_analyser.h
+++ src/analysis/i2c_timing_analyser.h
@@ -2,26 +2,13 @@
 // Released under the MIT License. See license.txt. (https://opensource.org/licenses/MIT)
 
 #pragma once
+
 #include <bus_trace/bus_trace.h>
 #include <analysis/i2c_timing_analysis.h>
 
 namespace analysis {
 
 class I2CTimingAnalyser {
-private:
-    // Time to rise from GND to 0.3 Vdd as a proportion of the rise time
-    constexpr static double RISE_V0_to_V0_3 = 0.421;
-    // Time to rise from 0.3 Vdd to the Teensy's trigger voltage (0.5 Vdd), as a proportion of the rise time
-    constexpr static double RISE_V0_3_to_TRIGGER = 0.397;
-    // Time to rise from the Teensy's trigger voltage (0.5 Vdd) to 0.7 Vdd, as a proportion of the rise time
-    constexpr static double RISE_TRIGGER_to_V0_7 = 0.603;
-
-    // Time to fall from Vdd to 0.7 Vdd as a proportion of the rise time
-    constexpr static double FALL_V1_to_V0_7 = RISE_V0_to_V0_3;
-    // Time to fall from 0.7 Vdd to the Teensy's trigger voltage (0.5 Vdd), as a proportion of the fall time
-    constexpr static double FALL_V0_7_to_TRIGGER = RISE_V0_3_to_TRIGGER;
-    // Time to fall from the Teensy's trigger voltage (0.5 Vdd) to 0.3 Vdd, as a proportion of the fall time
-    constexpr static double FALL_TRIGGER_to_V0_3 = RISE_TRIGGER_to_V0_7;
 public:
     static const uint16_t DEFAULT_FALL_TIME = 8;
 
@@ -32,17 +19,76 @@ public:
     // You can measure the rise and fall times with LineTester::TestLine()
     // or an oscilloscope.
     static I2CTimingAnalysis analyse(const bus_trace::BusTrace& trace,
-                                     uint16_t sda_rise_time, uint16_t scl_rise_time,
-                                     uint16_t sda_fall_time=DEFAULT_FALL_TIME, uint16_t scl_fall_time=DEFAULT_FALL_TIME);
+                                     uint16_t sda_rise_time,
+                                     uint16_t scl_rise_time,
+                                     uint16_t sda_fall_time = DEFAULT_FALL_TIME,
+                                     uint16_t scl_fall_time = DEFAULT_FALL_TIME);
 
 private:
-    static uint32_t adjust_setup_stop_time(uint32_t raw_time, uint16_t sda_rise_time, uint16_t scl_rise_time);
-    static uint32_t adjust_setup_start_time(uint32_t raw_time, uint16_t sda_fall_time, uint16_t scl_rise_time);
-    static uint32_t adjust_start_hold_time(uint32_t raw_time, uint16_t sda_fall_time, uint16_t scl_fall_time);
-    static uint32_t adjust_clock_low_time(uint32_t raw_time, uint16_t scl_fall_time, uint16_t scl_rise_time);
-    static uint32_t adjust_clock_high_time(uint32_t raw_time, uint16_t scl_rise_time, uint16_t scl_fall_time);
-    static uint32_t adjust_bus_free_time(uint32_t raw_time, uint16_t sda_rise_time, uint16_t sda_fall_time);
-    static uint32_t adjust_data_setup_time(uint32_t raw_time, bool sda_rose, uint16_t sda_rise_time, uint16_t sda_fall_time, uint16_t scl_rise_time);
+    class Adjuster {
+    public:
+        Adjuster(uint16_t sda_rise_time, uint16_t sda_fall_time, uint16_t scl_rise_time, uint16_t scl_fall_time);
+
+        uint32_t setup_stop_time(uint32_t raw_time) const;
+        uint32_t setup_start_time(uint32_t raw_time) const;
+        uint32_t start_hold_time(uint32_t raw_time) const;
+        uint32_t clock_low_time(uint32_t raw_time) const;
+        uint32_t clock_high_time(uint32_t raw_time) const;
+        uint32_t bus_free_time(uint32_t raw_time) const;
+        uint32_t data_setup_time(uint32_t raw_time, bool sda_rose) const;
+
+    private:
+        // Time to rise from GND to 0.3 Vdd as a proportion of the rise time
+        constexpr static double RISE_V0_to_V0_3 = 0.421;
+        // Time to rise from 0.3 Vdd to the Teensy's trigger voltage (0.5 Vdd), as a proportion of the rise time
+        constexpr static double RISE_V0_3_to_TRIGGER = 0.397;
+        // Time to rise from the Teensy's trigger voltage (0.5 Vdd) to 0.7 Vdd, as a proportion of the rise time
+        constexpr static double RISE_TRIGGER_to_V0_7 = 0.603;
+
+        // Time to fall from Vdd to 0.7 Vdd as a proportion of the rise time
+        constexpr static double FALL_V1_to_V0_7 = RISE_V0_to_V0_3;
+        // Time to fall from 0.7 Vdd to the Teensy's trigger voltage (0.5 Vdd), as a proportion of the fall time
+        constexpr static double FALL_V0_7_to_TRIGGER = RISE_V0_3_to_TRIGGER;
+        // Time to fall from the Teensy's trigger voltage (0.5 Vdd) to 0.3 Vdd, as a proportion of the fall time
+        constexpr static double FALL_TRIGGER_to_V0_3 = RISE_TRIGGER_to_V0_7;
+
+        double scl_rise_trigger_to_V0_7() const {
+            return scl_rise_time * RISE_TRIGGER_to_V0_7;
+        }
+
+        double scl_rise_V0_3_to_trigger() const {
+            return scl_rise_time * RISE_V0_3_to_TRIGGER;
+        }
+
+        double scl_fall_V0_7_to_trigger() const {
+            return scl_fall_time * FALL_V0_7_to_TRIGGER;
+        }
+
+        double scl_fall_trigger_to_V0_3() const {
+            return scl_fall_time * FALL_TRIGGER_to_V0_3;
+        }
+
+        double sda_rise_trigger_to_V0_7() const {
+            return sda_rise_time * RISE_TRIGGER_to_V0_7;
+        }
+
+        double sda_rise_V0_3_to_trigger() const {
+            return sda_rise_time * RISE_V0_3_to_TRIGGER;
+        }
+
+        double sda_fall_V0_7_to_trigger() const {
+            return sda_fall_time * FALL_V0_7_to_TRIGGER;
+        }
+
+        double sda_fall_trigger_to_V0_3() const {
+            return sda_fall_time * FALL_TRIGGER_to_V0_3;
+        }
+
+        uint16_t sda_rise_time;
+        uint16_t sda_fall_time;
+        uint16_t scl_rise_time;
+        uint16_t scl_fall_time;
+    };
 };
 
 } // analysis
